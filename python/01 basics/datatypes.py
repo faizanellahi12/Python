be332241dd20320117ebef993d98cebd6ad6202b
Faizan <faizane619@gmail.com>
2025-03-18 18:03:57 +0530

    Add files via upload

---
+++ python/01 basics/datatypes.py
@@ -0,0 +1,32 @@
+data types/object types 
+# Integer
+a = 10
+print(type(a))  # Output: <class 'int'>
+
+# Float
+b = 10.5
+print(type(b))  # Output: <class 'float'>
+
+# String
+c = "Hello"
+print(type(c))  # Output: <class 'str'>
+
+# List
+d = [1, 2, 3]
+print(type(d))  # Output: <class 'list'>
+
+# Tuple
+e = (1, 2, 3)
+print(type(e))  # Output: <class 'tuple'>
+
+# Dictionary
+f = {"name": "John", "age": 30}
+print(type(f))  # Output: <class 'dict'>
+
+# Set
+g = {1, 2, 3}
+print(type(g))  # Output: <class 'set'>
+
+# Boolean
+h = True
+print(type(h))  # Output: <class 'bool'>
---
+++ python/01 basics/hello_python.py
@@ -0,0 +1,15 @@
+# print("hello chaii")
+# def chaai(n):
+#     print (n)
+
+
+ 
+
+# # chaai("hilal")
+# for a in "apple":
+#  print(a)
+
+mutable and immmutable
+
+
+
