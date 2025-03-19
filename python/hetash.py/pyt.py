95317f420913b09702044891f58b87f13fe8dc95
Faizan <faizane619@gmail.com>
2025-03-19 22:42:05 +0530

    Add files via upload

---
+++ python/hetash.py/pyt.py
@@ -0,0 +1,28 @@
+# # import math
+# # # print(math.pi)
+
+
+# string
+# # username="noonchza";
+# # print(len(username));
+# # # print("username")
+# # #  print("username[1]")
+
+# list
+# # mylist=[123,"chaii",3.12,123]
+
+# # print(len(mylist))
+
+# tuple
+ 
+# # mytup=(1,2,3,3,4)
+# # print(len(mytup))
+
+
+# import sys 
+# print(sys.getrefcount(2345))
+
+
+a=3;
+a="chaii";
+print(a);
